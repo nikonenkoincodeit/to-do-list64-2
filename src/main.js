d2bdbf157322d66ae9a0e84837eded1d8e18575a
Heveliev-destress <tolik05.03@ukr.net>
2022-12-10 14:57:09 +0200

    add evenlisten

--- src/main.js
+++ src/main.js
@@ -1,2 +1,15 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/style.css";
+
+const formHtml = document.querySelector('.form');
+
+formHtml.addEventListener('submit', foo);
+
+function foo (evt) {
+evt.preventDefault();
+const value = evt.target.message.value.trim()
+if(!value){
+return
+}
+console.log(value)
+}
